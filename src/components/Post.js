ca5c404e0ab9d0fa73a201d70661435e7357989e
Ty <thopp@polity.tech>
2017-10-26 17:03:11 -0700

    finished post cards JSX scaffolding

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+// icons
+import FaCommenting from 'react-icons/lib/fa/commenting'
+import FaThumbsUp from 'react-icons/lib/fa/thumbs-up'
+import FaThumbsDown from 'react-icons/lib/fa/thumbs-down'
+import MdAccountCircle from 'react-icons/lib/md/account-circle'
+
 // project styles
 import '../styles/All.css'
 import '../styles/Post.css';
@@ -9,7 +15,44 @@ class Post extends Component {
     return (
       <div>
         <div className="postContainer">
-        	A post
+        	<div className="postRow">
+        		<div className="postTitle">
+        			Post Title
+        		</div>
+        	</div>
+        	<div className="postRow">
+        		<div className="postAuthorIcon">
+        			<MdAccountCircle />
+        		</div>
+        		<div className="postAuthorName">
+        			Jane
+        		</div>
+        	</div>
+        	<div className="postRow">
+        		<div className="actionList">
+        			<div className="actionItem">
+	        			<div className="actionCommentCount">
+	        				2
+	        			</div>
+	        			<div className="actionComment">
+	        				<FaCommenting />
+	        			</div>
+	        		</div>
+	        		<div className="actionItem">
+	        			<div className="actionThumbsUp">
+	        				<FaThumbsUp />
+	        			</div>
+	        		</div>
+	        		<div className="actionItem">
+	        			<div className="actionThumbsDown">
+	        				<FaThumbsDown />
+	        			</div>
+	        		</div>
+        		</div>
+        		<div className="postCategory">
+        			Category
+        		</div>
+        	</div>
         </div>
       </div>
     );
