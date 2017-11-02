482c2ee8fe0c9b3901559b9e02cbd35a85e256ee
Ty <thopp@polity.tech>
2017-11-02 11:08:06 -0700

    finished implementing vanilla Redux, now switchingt to React-redux

--- src/components/Quoted.js
+++ src/components/Quoted.js
@@ -10,13 +10,18 @@ import '../styles/All.css'
 
 
 class Quoted extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Route exact path="/" render={() => ( <Home /> )} />
-      </div>
-    );
-  }
+
+	state = {
+		postId: null,
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<Route exact path="/" render={() => ( <Home /> )} />
+			</div>
+		);
+	}
 }
 
 export default Quoted;
--- src/index.js
+++ src/index.js
@@ -3,23 +3,24 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import Quoted from './components/Quoted';
+import registerServiceWorker from './registerServiceWorker';
+
 // import store from './store';
 import reducer from './reducers'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
 	reducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
-console.log('This is the store: ' + store)
+console.log(store.getState())
 
 
 ReactDOM.render(
 	<BrowserRouter>
 {/*		<Provider store={store}> 	*/}
-			<Quoted />
+			<Quoted store={store}/>
 {/*		</Provider>					*/}
 	</BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
